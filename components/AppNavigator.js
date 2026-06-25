53f0618acbe6293cc42896a9c972c57f3cbda921
Harini672 <harinibalaji672@gmail>
2026-06-25 21:09:25 +0530

    passrequest

--- components/AppNavigator.js
+++ components/AppNavigator.js
@@ -1,11 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import PassRequestsScreen from './PassrequestsScreen';
 
-import HomeScreen from './HomeScreen';
-import LoginScreen from './LoginScreen';
-import DashboardScreen from './DashboardScreen';
-import GoogleAuthScreen from './GoogleAuthScreen';
-
+//import HomeScreen from './HomeScreen';
+//import LoginScreen from './LoginScreen';
+//import DashboardScreen from './DashboardScreen';
+//import GoogleAuthScreen from './GoogleAuthScreen';
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
@@ -13,25 +13,26 @@ export default function AppNavigator() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
 
-        <Stack.Screen
+        {/* <Stack.Screen
           name="Home"
           component={HomeScreen}
-        />
+        /> */}
 
-        <Stack.Screen
+        {/* <Stack.Screen
           name="Login"
           component={LoginScreen}
-        />
+        /> */}
 
-        <Stack.Screen
+        {/* <Stack.Screen
           name="GoogleAuth"
           component={GoogleAuthScreen}
-        />
+        /> */}
 
-        <Stack.Screen
+        {/* <Stack.Screen
           name="Dashboard"
           component={DashboardScreen}
-        />
+        /> */}
+        <Stack.Screen name="PassRequests" component={PassRequestsScreen} />
 
       </Stack.Navigator>
     </NavigationContainer>
